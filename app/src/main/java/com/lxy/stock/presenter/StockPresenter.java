fdabba0f2c4101e4fc44391acc5396760160dbef
lxykad <503215875@qq.com>
2016-06-22 22:52:37 +0800

    bug fix

--- app/src/main/java/com/lxy/stock/presenter/StockPresenter.java
+++ app/src/main/java/com/lxy/stock/presenter/StockPresenter.java
@@ -1,9 +1,16 @@
 package com.lxy.stock.presenter;
 
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+
 import com.lxy.stock.common.CommonResponseHandler;
 import com.lxy.stock.module.Stock;
 import com.lxy.stock.utils.HSJsonUtil;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
@@ -11,17 +18,57 @@ import java.util.ArrayList;
  */
 public class StockPresenter {
 
+    private static final int GROUP_SUCCESS = 1;
+    private Context mContext;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            //super.handleMessage(msg);
+            switch (msg.what) {
+                case GROUP_SUCCESS:
+                    String jsonData = (String) msg.obj;
+                    ArrayList<Stock> stockList = HSJsonUtil.getRealStockList(jsonData, "Messages");
+                    System.out.println("11111111111==========list===="+stockList.size());
+
+//                    if (stockList.size() == 0) {
+//                        //数据解析失败
+//                        handler.onFailure();
+//                    } else {
+//                        //数据解析成功
+//                        handler.onLoadGroupStockSuccess(stockList);
+//                    }
+
+                    break;
+            }
+        }
+    };
+
+    public StockPresenter(Context context) {
+        mContext = context;
+    }
+
     //Thread
+
     //获取组合股票的数据
     public void getGroupStockData(CommonResponseHandler handler) {
-        ArrayList<Stock> stockList = HSJsonUtil.getRealStockList("", "data.json");
-        if (stockList.size() == 0) {
-            //数据解析失败
-            handler.onFailure();
-        } else {
-            //数据解析成功
-            handler.onLoadGroupStockSuccess(stockList);
-        }
+
+        //读取json数据，放在子线程
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String jsonData = loadJsonDataFromAssets("data.json");
+                Message msg = mHandler.obtainMessage();
+                msg.what = GROUP_SUCCESS;
+                msg.obj = jsonData;
+                mHandler.sendMessage(msg);
+
+            }
+        }).start();
+
+
+
+
+
     }
 
     //获取单个股票的数据
@@ -35,4 +82,24 @@ public class StockPresenter {
             handler.onLoadSingleStockSuccess(stock);
         }
     }
+
+    //解析本地json数据
+    protected String loadJsonDataFromAssets(String fileName) {
+
+        StringBuffer buffer = new StringBuffer();
+        try {
+            InputStreamReader inputReader = new InputStreamReader(mContext.getAssets().open(fileName));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            while ((line = bufReader.readLine()) != null) {
+                buffer.append(line);
+            }
+        } catch (IOException e) {
+            //
+            e.printStackTrace();
+        }
+        return buffer.toString();
+    }
+
+
 }
--- app/src/main/java/com/lxy/stock/ui/MainActivity.java
+++ app/src/main/java/com/lxy/stock/ui/MainActivity.java
@@ -11,6 +11,9 @@ import com.lxy.stock.presenter.StockPresenter;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        mPresenter = new StockPresenter();
+        mPresenter = new StockPresenter(this);
     }
 
     //获取stock的集合
@@ -37,13 +40,17 @@ public class MainActivity extends AppCompatActivity {
         mPresenter.getGroupStockData(new CommonResponseHandler(){
             @Override
             public void onFailure() {
-
+                System.out.println("11111111111==========failure");
             }
 
             @Override
             public void onLoadGroupStockSuccess(ArrayList<Stock> list) {
-
+                System.out.println("11111111111==========success");
             }
         });
     }
+
+
+
+
 }
